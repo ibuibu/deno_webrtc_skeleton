caffa9d4b97b8725e963b8f83acd01188a3f4ed3
ibuibu <ibuibu69@gmail.com>
2023-05-04 10:09:40 +0900

    mesh

--- client.js
+++ client.js
@@ -1,7 +1,10 @@
 function createWebSocket() {
-  const websocket = new WebSocket("ws://localhost:8000")
+  // const websocket = new WebSocket("ws://localhost:8000")
+  // const websocket = new WebSocket("ws://ibuibu-deno-ws-test.deno.dev")
+  const websocket = new WebSocket("wss://ibuibu-deno-ws-test.deno.dev")
 
   websocket.onopen = () => {
+    console.log('hoge')
     const num = Math.random();
     setInterval(() => {
       websocket.send(`Client says hello ${num}`)
@@ -11,6 +14,10 @@ function createWebSocket() {
 
   websocket.onmessage = (message) => {
     console.log(message.data)
+  }
+
+  websocket.onerror = (message)=>{
+    console.log({message})
   }
 }
 
--- main.js
+++ main.js
@@ -1,11 +1,45 @@
-import { serve } from 'https://deno.land/std@0.156.0/http/server.ts';
+import { serve } from "https://deno.land/std@0.156.0/http/server.ts";
 
-serve((req) => {
-  const { response, socket } = Deno.upgradeWebSocket(req);
-  socket.addEventListener('message', (event) => {
-    console.log(event.data);
-    socket.send("aaa")
-  });
+const rooms = new Map();
 
-  return response;
-}, { port: parseInt(Deno.env.get('PORT') ?? '') || 443 });
+serve(
+  (req) => {
+    const { response, socket: ws } = Deno.upgradeWebSocket(req);
+
+    ws.addEventListener("message", (event) => {
+      const mes = JSON.parse(event.data);
+
+      if (mes.type === "join") {
+        const members = rooms.get(mes.roomId);
+        // console.log({ wss: members });
+        if (members) {
+          members.forEach((member) => {
+            member.send(JSON.stringify({ type: "offer", sdp: mes.sdp }));
+          });
+
+          members.push(ws);
+          rooms.set(mes.roomId, members);
+        } else {
+          rooms.set(mes.roomId, [ws]);
+        }
+        // console.log({ rooms });
+      }
+
+      if (mes.type === "answer") {
+        const members = rooms.get(mes.roomId);
+        members.forEach((member) => {
+          if (member !== ws) {
+            member.send(JSON.stringify({ type: "answer", sdp: mes.sdp }));
+          }
+        });
+      }
+    });
+
+    ws.onclose = () => {
+
+    };
+
+    return response;
+  },
+  { port: parseInt(Deno.env.get("PORT") ?? "") || 443 }
+);
